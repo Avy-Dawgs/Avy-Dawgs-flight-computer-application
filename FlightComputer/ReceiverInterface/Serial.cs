80bdf4325b0df64fa986287277b3534c036cc1bd
Aidan Leary <learyaidan@gmail.com>
2025-07-30 22:50:27 -0400

    initial implementation of interface to receiver + a simple test for that interface

---
+++ FlightComputer/ReceiverInterface/Serial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ReceiverInterface;
+
+public class ReceiverInterface : IDisposable
+{
+    private readonly SerialPort _serialPort;
+
+    private readonly CancellationTokenSource _cts;
+
+    private Task? _receiveTask;
+
+    public event EventHandler<string>? ReceivedData;
+    
+    public ReceiverInterface(string portName)
+    {
+        _serialPort = new SerialPort()
+        {
+            PortName = portName,
+            BaudRate = 115200, 
+            Parity = Parity.None, 
+            DataBits = 8, 
+            StopBits = StopBits.One, 
+            Handshake = Handshake.None, 
+        };
+
+        _cts = new();
+    }
+
+    public void Start()
+    {
+        if (_serialPort.IsOpen)
+        {
+            return;
+        }
+        
+        _serialPort.Open();
+        
+        _receiveTask = Task.Run(() => ReadLoop(_cts.Token));
+    }
+
+    private void ReadLoop(CancellationToken token)
+    {
+        while (true)
+        {
+            if (token.IsCancellationRequested)
+            {
+                break;
+            }
+
+            string readString = _serialPort.ReadExisting();
+            if (readString.Length > 0)
+            {
+                ReceivedData?.Invoke(this, readString);
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        _cts.Cancel();
+        _receiveTask?.Wait();
+    }
+}
---
+++ FlightComputer/ReceiverTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using ReceiverInterface;
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Starting.");
+
+ReceiverInterface.ReceiverInterface receiver = new(args[0]);
+
+receiver.ReceivedData += Handler;
+receiver.Start();
+
+while (true)
+{
+    
+}
+
+void Handler(object? sender, string receivedData)
+{
+    Console.Write(receivedData);
+}
